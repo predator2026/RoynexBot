eaef927f03c0dcea2259b28ba64d63671061d864
predator2026 <artur.arm.12@mail.ru>
2025-07-04 07:35:34 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает 24/7 🔥")
+
+app = ApplicationBuilder().token("8153276855:AAEqgDMBDCsQBwlzIdB7iMtO-g6E1SWwTtg").build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
