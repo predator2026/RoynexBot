bfc0e8e250e336239a182e4b3348282c406a10b8
predator2026 <artur.arm.12@mail.ru>
2025-07-05 08:26:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import os
-import logging
+from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ContextTypes
 
-# Логирование
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-# Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Բարև, սա RoynexBot է։ Ընտրեք կոճակ։")
+    await update.message.reply_text("Բարեւ, սա RoynexBot-ն է 🔐")
 
-# Главная функция
 def main():
-    application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
+    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
