fdc3929868403016d9a11426b787d51500aaaf70
predator2026 <artur.arm.12@mail.ru>
2025-07-05 07:15:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-import os
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    CallbackQueryHandler,
-    ContextTypes,
-    filters,
-)
-
-TOKEN = os.getenv("BOT_TOKEN")
+mport os
+import asyncio
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        [InlineKeyboardButton("📸 Կարծիքներ Roynex-ի մասին", callback_data="reviews")],
-        [InlineKeyboardButton("💼 Մյուս նախագծեր", callback_data="projects")],
-        [InlineKeyboardButton("💎 Գնել VIP", callback_data="buy_vip")],
-        [InlineKeyboardButton("💰 Լիցքավորել հաշիվ", callback_data="top_up")]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Բարի գալուստ RoyNex Bot 🔐", reply_markup=reply_markup)
+    await update.message.reply_text("Բարև, սա RoynexBot է 🚀")
 
-async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    if query.data == "reviews":
-        await query.edit_message_text("📸 Ահա Roynex-ի կարծիքները...")
-    elif query.data == "projects":
-        await query.edit_message_text("💼 Այստեղ կգտնես մեր մյուս նախագծերը...")
-    elif query.data == "buy_vip":
-        await query.edit_message_text("💎 Գնելու համար VIP՝ փոխանցիր 45000 Դրամ այս համարին՝\n\n**4355 0539 2271 9150**")
-    elif query.data == "top_up":
-        await query.edit_message_text("💰 Լիցքավորման համար ուղարկիր սքրին՝ վճարման մասին: Մենք կհաստատենք հաշիվը:")
-
-if __name__ == '__main__':
-    app = ApplicationBuilder().token(TOKEN).build()
+async def main():
+    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+    
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CallbackQueryHandler(button_handler))
-    app.run_polling()
+
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
