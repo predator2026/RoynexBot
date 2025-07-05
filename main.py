c801949a88ac8c3a180a78965b28dbd9bf290a5d
predator2026 <artur.arm.12@mail.ru>
2025-07-05 05:59:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Բարեւ։ Սա RoynexBot է։")
+    await update.message.reply_text("Բարի գալուստ RoynexBot 📍")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token("TOKEN").build()
-    
+    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
