6face086f4a119252577d243365a504b31febd9b
predator2026 <artur.arm.12@mail.ru>
2025-07-05 05:51:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Բարեւ։ Սա RoynexBot է։")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token("ТВОЙ_ТОКЕН").build()
+    app = ApplicationBuilder().token("TOKEN").build()
     
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
